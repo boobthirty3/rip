543cd9e568819e2c187a15ed26d219af638557cd
boobthirty3 <120739267+boobthirty3@users.noreply.github.com>
2022-12-17 17:14:12 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function init() {
 
   //Camera setup
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 5, 30);
+  camera.position.set(-3, 0, 6);
 
   const ambient = new THREE.AmbientLight(0x404040, 2);
   scene.add(ambient);
@@ -43,6 +43,8 @@ function init() {
     animate();
   });
 }
+
+
 
 
 function animate() {
