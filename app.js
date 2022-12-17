5c10f34e5e6ec02f3f7f24d5023a3d41c98f049a
boobthirty3 <120739267+boobthirty3@users.noreply.github.com>
2022-12-17 17:19:06 +0000

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function init() {
 
   //Camera setup
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(-3, 0, 6);
+  camera.position.set(-2, 0, 7);
 
   const ambient = new THREE.AmbientLight(0x404040, 2);
   scene.add(ambient);
