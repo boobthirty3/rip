f8890a1dfd0139b73cc3ae55a49a64a340ee8b8a
boobthirty3 <120739267+boobthirty3@users.noreply.github.com>
2022-12-19 21:12:00 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function init() {
 
   //Load Model
   let loader = new THREE.GLTFLoader();
-  loader.load("./dolltit.gltf", function(gltf) {
+  loader.load("./dolltitsmooth.gltf", function(gltf) {
     scene.add(gltf.scene);
     doll = gltf.scene.children[0];
     animate();
