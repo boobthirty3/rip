cd022f93bbb15e142b52cda3893eee79552bd81f
boobthirty3 <120739267+boobthirty3@users.noreply.github.com>
2023-02-25 13:48:51 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,87 +1,42 @@
+const intro = document.querySelector('.intro');
+const video = intro.querySelector('video');
+const text = intro.querySelector('h1');
+
+const section = document.querySelector('section');
+const end = section.querySelector('h1')
+
+const controller = new ScrollMagic.Controller();
+
+let scene = new ScrollMagic.Scene({
+  duration: 20000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+.addIndicators()
+.setPin(intro)
+.addTo(controller);
 
 
-//Variables for setup
+const textAnim = TweenMax.fromTo(text, 3, {opacity:1}, {opacity:0});
 
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook:0
+})
+.setTween(textAnim)
+.addTo(controller);
 
-let container;
-let camera;
-let renderer;
-let scene;
-let doll;
-let OrbitControls;
+let accelamount = 0.9;
+let scrollpos =0;
+let delay = 0;
 
-function init() {
-  container = document.querySelector(".scene");
+scene.on("update", e =>{
+  scrollpos = e.scrollPos / 1000;
+})
 
-  //Create scene
-  scene = new THREE.Scene();
+setInterval(()=>{
+  delay += (scrollpos - delay) * accelamount;
 
-  const fov = 35;
-  const aspect = container.clientWidth / container.clientHeight;
-  const near = 0.1;
-  const far = 1000;
-
-  //Camera setup
-  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(-2, 1, 7);
-
-  const ambient = new THREE.AmbientLight(0x404040, 2);
-  scene.add(ambient);
-
-  const light = new THREE.DirectionalLight(0xdddddd, 2);
-  light.position.set(-1000, 50, 7000);
-  scene.add(light);
-  
-  //Renderer
-  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-  renderer.setSize(container.clientWidth, container.clientHeight);
-  renderer.setPixelRatio(window.devicePixelRatio);
-
-  container.appendChild(renderer.domElement);
-
-  //Load Model
-  let loader = new THREE.GLTFLoader();
-  loader.load("./newnew.gltf", function(gltf) {
-    scene.add(gltf.scene);
-    doll = gltf.scene.children[0];
-    animate();
-  });
-
-  const controls = new THREE.OrbitControls( camera, renderer.domElement );
-				 // use if there is no animation loop
-				controls.minDistance = 400;
-				controls.maxDistance = 1000;
-				controls.target.set( 10, 90, - 16 );
-				controls.update();
-        window.addEventListener( 'resize', onWindowResize );
-}
-
-
-
-function animate() {
-  requestAnimationFrame(animate);
-  doll.rotation.z += 0.005;
-  doll.rotation.y += 0.01;
-  doll.rotation.x += 0.005;
-  renderer.render(scene, camera);
-
-  
-}
-
-
-init();
-
-function onWindowResize() {
-  camera.aspect = container.clientWidth / container.clientHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(container.clientWidth, container.clientHeight);
-}
-
-window.addEventListener("resize", onWindowResize);
-
-
-
-
-
-
+  video.currentTime = delay;
+}, 33.3);
